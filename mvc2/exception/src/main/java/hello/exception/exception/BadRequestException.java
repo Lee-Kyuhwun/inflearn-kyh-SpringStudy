8222a95829b54c36d3574002f36c2e68648c8c18
Lee-KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2023-04-23 23:03:14 +0900

    Add files via upload

---
+++ mvc2/exception/src/main/java/hello/exception/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package hello.exception.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 오류")
+public class BadRequestException extends  RuntimeException{
+
+
+
+}
