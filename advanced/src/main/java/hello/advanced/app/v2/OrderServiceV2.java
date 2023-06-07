89b506bf24eee797d165954532d508a7f1585385
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2023-06-07 12:00:00 +0900

    feat: 스프링 고급 로그 추적기 V2 로그레벨표현 및 테스트 코드 작성

--- advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
+++ advanced/src/main/java/hello/advanced/app/v2/OrderServiceV2.java
@@ -1,17 +1,16 @@
 package hello.advanced.app.v2;
 
 
-import hello.advanced.app.v1.OrderRepositoryV1;
 import hello.advanced.trace.TraceStatus;
-import hello.advanced.trace.hellotrace.HelloTraceV1;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceV2 {
-    private final OrderRepositoryV1 orderRepository;
-    private final HelloTraceV1 trace;
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
 
 
     public void orderItem(String itemId){
