353206898308921a271c8b108d2d64099b47b557
LeeKyuHwun <wxyz0904@naver.com>
2025-05-11 16:33:02 +0900

    feat: 스프링 고급 데코레이터 패턴 예제 코드 3

--- proxy/src/test/java/hello/proxy/decorator/code/DecoratorPatternTest.java
+++ proxy/src/test/java/hello/proxy/decorator/code/DecoratorPatternTest.java
@@ -23,4 +23,13 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
+
+    @Test
+    void decorator2() {
+        Component component = new RealComponent();
+        Component messageDecorator = new MessageDecorator(component);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
 }
