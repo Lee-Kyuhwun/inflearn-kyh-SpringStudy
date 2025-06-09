903080c14335c93748a0029d773186bf384e6443
LeeKyuHwun <wxyz0904@naver.com>
2025-06-09 23:15:46 +0900

    feat: 스프링 고급 동적프록시

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import hello.proxy.app.config.AppV1Config;
 import hello.proxy.app.config.AppV2Config;
 import hello.proxy.app.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.app.config.v1_proxy.concrete_proxy.ConcreteProxyConfig;
+import hello.proxy.app.config.v1_proxy.v2_proxy.handler.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +14,10 @@ import org.springframework.context.annotation.Import;
 
 
 //@Import({InterfaceProxyConfig.class})
-@Import({ConcreteProxyConfig.class, AppV2Config.class}) //주의
-@SpringBootApplication(scanBasePackages = "hello.proxy.app.v2") //주의
+//@Import({ConcreteProxyConfig.class, AppV2Config.class}) //주의
+//@SpringBootApplication(scanBasePackages = "hello.proxy.app.v2") //주의
+@Import(DynamicProxyBasicConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
